347e909ffe83a7edf81e11531b13bd446ed72256
LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
2024-01-01 13:57:25 +0100

    Squashed commit of the following:
    commit e734647b42e9556d60a6329b89514d02c25dc5b4
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Mon Jan 1 13:52:22 2024 +0100
    
        lllllllllll
    
    commit 22478db02ec9fa7f3d0610081f3d1b75c7944c81
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Mon Jan 1 13:44:53 2024 +0100
    
        ooooooooooooo
    
    commit cf22fc60f7ce711ca07e149545d9041e395659d2
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Mon Jan 1 13:37:35 2024 +0100
    
        0000000
    
    commit a820ccc2d9ee50bc55a42e87f268155003ab81d0
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Mon Jan 1 13:05:47 2024 +0100
    
        oooooo
    
    commit 6a3a3b96e1e5b66980dee5df4dea12f0f4dd0ef4
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sun Dec 31 20:32:43 2023 +0100
    
        ooooo
    
    commit cc46150de0e546fd8ec0952dba57c5912ebb8183
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sun Dec 31 19:57:24 2023 +0100
    
        kk
    
    commit 782bcb4e794b4cafbbb6906ad6779355e2411b77
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sun Dec 31 08:32:43 2023 +0100
    
        ppppppp
    
    commit e4c5a5efd72fe8819fd5cc847e1a37c3aed57c7a
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 21:26:36 2023 +0100
    
        00000
    
    commit fcd69219e25a2df49d2623d31378bd5b78d358fa
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 20:54:14 2023 +0100
    
        ooooo
    
    commit 0c22018577fae4644d317eb2ed97503d8f95d563
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 20:22:44 2023 +0100
    
        ooo
    
    commit 02967afcc85ce983574618834a969bbbcfb5bb6e
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 19:54:42 2023 +0100
    
        oo
    
    commit c53f667f0ddd8193927c860ddc942236020614f4
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 16:14:16 2023 +0100
    
        kkkkk
    
    commit 50518a138e50e90668f4ac2339ce726b5c5ecc07
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 14:43:25 2023 +0100
    
        Update idee.html
    
    commit eeb0a502df675d9a871d940fff99a6b3965a6920
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 13:07:06 2023 +0100
    
        kkkk
    
    commit a2b0e5014b1470fbd1f5156a1894f30356e8026a
    Merge: d858a3e 7619f31
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 13:05:39 2023 +0100
    
        Merge branch 'metcode' of https://github.com/LadyoftheCave/high_fidelity into metcode
    
    commit d858a3e2f153ce5c3cf909aacd44cbb9e3e0d20c
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 13:05:36 2023 +0100
    
        kk
    
    commit 7619f31ea897fcf87cf26274281057a2e8edc776
    Author: drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
    Date:   Sat Dec 30 11:39:46 2023 +0100
    
        upload knop bestanden weg
    
    commit 19336777b48c23ed97227c3c4850960fbec345a4
    Author: drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
    Date:   Sat Dec 30 11:37:55 2023 +0100
    
        javascript in de js bestanden gezet
    
    commit 70472806c3fafda3cd197a9fbb979b6e18f49099
    Author: LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
    Date:   Sat Dec 30 11:03:41 2023 +0100
    
        ll

---
+++ js/controller.js
@@ -0,0 +1,64 @@
+function saveIdeaForm() {
+  // Verkrijg formuliergegevens
+  const formData = {
+    onderwerp: document.getElementById("tekstveld").value,
+    idee: document.getElementById("tekstarea").value,
+    afdeling: document.getElementById("selectie").value,
+  };
+
+  // Ophalen opgeslagen formulieren
+  const storedForms = JSON.parse(localStorage.getItem("forms")) || [];
+
+  // Toevoegen van nieuwe formuliergegevens
+  storedForms.push(formData);
+
+  // Opnieuw opslaan van alle formulieren
+  localStorage.setItem("forms", JSON.stringify(storedForms));
+
+  // Doorsturen naar andere pagina
+  window.location.href = "idee_overzicht.html";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Haal de waarde van de 'index' query parameter op
+  const urlParams = new URLSearchParams(window.location.search);
+  const index = urlParams.get("index");
+
+  // Ophalen opgeslagen formulieren uit localStorage
+  const storedForms = JSON.parse(localStorage.getItem("forms")) || [];
+
+  // Controleren of er gegevens zijn opgeslagen en of de index bestaat
+  if (storedForms && storedForms.length > 0 && index !== null) {
+    // Haal de specifieke gegevens op voor het formulier met de opgegeven index
+    const formData = storedForms[index];
+
+    // Selecteer de container voor resultaten
+    const resultatenDiv = document.getElementById("resultaten");
+
+    const width = 1008; // Pas de breedte aan naar het gewenste formaat
+    const height = 400; // Pas de hoogte aan naar het gewenste formaat
+
+    fetch(`https://picsum.photos/${width}/${height}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.url;
+      })
+      .then(imageUrl => {
+        // Voeg het specifieke idee toe aan de container
+        resultatenDiv.innerHTML = `
+                      <div class="resultaat">
+  
+                          <h2>${formData.onderwerp}</h2><br>
+                          <img src="${imageUrl}" alt="Random Image">
+                          <p id="demo"></p>
+                          
+                          <p><br>${formData.idee}</p>
+  
+                      </div>
+                  `;
+      })
+      .catch(error => console.error("Fetch error:", error));
+  }
+});
